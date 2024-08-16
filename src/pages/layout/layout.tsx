071730decf86f64994e396d68a1bbdcdfa9212ec
so1yana <trapbigest@gmail.com>
2024-08-16 17:23:28 +0300

    video rendering

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import BgVideo from 'src/components/BgVideo';
 import Header from 'src/components/Header';
 import Navigation from 'src/components/Navigation';
 
@@ -9,6 +10,7 @@ const Layout: FC = () => {
     <>
       <Header />
       <Navigation />
+      <BgVideo />
       <Outlet />
     </>
   );

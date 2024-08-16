4a17943b8bb81b7f811d0e7874dd1c1f84e7816c
so1yana <trapbigest@gmail.com>
2024-08-16 17:23:21 +0300

    classname support added

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -4,10 +4,15 @@ import './Button.scss';
 
 interface IButton {
   children: string;
+  className?: string;
 }
 
-const Button: FC<IButton> = ({ children }) => {
-  return <button className="base-button">{children}</button>;
+const Button: FC<IButton> = ({ children, className }) => {
+  return (
+    <button className={`base-button${className ? ' ' + className : ''}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;

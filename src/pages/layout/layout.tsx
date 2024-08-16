2e182b82b460c0c90b70b677a857b3dd2a5fcc7c
so1yana <trapbigest@gmail.com>
2024-08-16 17:28:12 +0300

    added main wrapper

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import BgVideo from 'src/components/BgVideo';
 import Header from 'src/components/Header';
 import Navigation from 'src/components/Navigation';
 
@@ -10,8 +9,9 @@ const Layout: FC = () => {
     <>
       <Header />
       <Navigation />
-      <BgVideo />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };

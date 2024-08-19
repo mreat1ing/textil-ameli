16a72e8d5044ae79bc77e2c3e798740e3ad66721
so1yana <trapbigest@gmail.com>
2024-08-19 21:48:00 +0300

    initial commit

---
+++ src/components/ContactUs/ContactUs.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+import Number from 'src/ui/number';
+
+import './ContactUs.scss';
+
+const ContactUs: FC = () => {
+  return (
+    <div className="contact-us">
+      <div className="contact-us__wrapper">
+        <div className="contact-us__content">
+          <h3 className="contact-us__title">Есть вопросы?</h3>
+          <p className="contact-us__description">Задайте их прямо сейчас!</p>
+          <Number className="contact-us__number" value="8 978 828 05 25" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;

219796ebdd2f21ed75570a8400bc5a37a91a7edd
so1yana <trapbigest@gmail.com>
2024-08-19 20:02:20 +0300

    Order rendering

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 
 import AboutCompany from 'src/components/AboutCompany';
 import BgVideo from 'src/components/BgVideo';
+import Order from 'src/components/Order';
 
 const Main: FC = () => {
   return (
     <>
       <BgVideo />
       <AboutCompany />
+      <Order />
     </>
   );
 };

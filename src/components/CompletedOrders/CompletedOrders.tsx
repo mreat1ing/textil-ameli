e5e442b7beaff1cd7e0920b654a82ec33b1989af
so1yana <trapbigest@gmail.com>
2024-08-19 21:26:06 +0300

    initial commit

---
+++ src/components/CompletedOrders/CompletedOrders.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import placeholder from 'src/assets/img/item-placeholder.png';
+import Button from 'src/ui/button';
+
+import './CompletedOrders.scss';
+
+const CompletedOrders: FC = () => {
+  return (
+    <div className="our-projects">
+      <div className="our-projects__wrapper">
+        <h2 className="our-projects__title">
+          Посмотрите наши выполненные проекты
+        </h2>
+        <ul className="our-projects__content">
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+          <li className="our-projects__item">
+            <img src={placeholder} alt="card" />
+          </li>
+        </ul>
+        <Link to="/our-works">
+          <Button>Показать еще</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedOrders;

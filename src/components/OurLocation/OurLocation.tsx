558583f68bd36f77dc174ae32e743103d1b86fbf
so1yana <trapbigest@gmail.com>
2024-08-21 18:33:39 +0300

    style changes

--- src/components/OurLocation/OurLocation.tsx
+++ src/components/OurLocation/OurLocation.tsx
@@ -13,12 +13,13 @@ const OurLocation: FC = () => {
         <Instagram
           className="our-location__instagram"
           description="пишите в direct"
+          width="35"
+          height="35"
         />
       </div>
       <iframe
         className="our-location__map"
         src="https://yandex.ru/map-widget/v1/?um=constructor%3Ae7085e432aba851ddee287ae6408e607f4facd57fdf8f8c75fe76795e3150e89&amp;source=constructor"
-        height="547"
         title="amelie"
       />
     </div>

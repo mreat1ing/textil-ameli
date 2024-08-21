a2be13fc5afee0cf11a0b3a642c0b0dbca1b329c
so1yana <trapbigest@gmail.com>
2024-08-21 18:33:09 +0300

    resize listener initial

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,80 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import BurgerMenu from 'src/components/BurgerMenu';
+import burgerToggle from 'src/utils/burgerToggle.utils';
+import CloseButton from 'src/ui/closeButton';
+import Instagram from 'src/ui/instagram';
 
 import './Navigation.scss';
 
+type navType = 'header' | 'footer' | 'burger';
+
 interface INavigation {
-  type?: 'header' | 'footer';
+  type?: navType;
 }
 
-const Navigation: FC<INavigation> = ({ type }) => {
+const Navigation: FC<INavigation> = ({ type = 'header' }) => {
+  const [navType, setNavType] = useState<navType>(type);
+
+  const handleResize = useCallback(() => {
+    if (window.innerWidth <= 990) {
+      setNavType('burger');
+    } else setNavType(type);
+  }, [type]);
+
+  useEffect(() => {
+    if (type === 'footer') return;
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => removeEventListener('resize', handleResize);
+  }, [handleResize, type]);
+
+  if (navType === 'burger') {
+    return (
+      <div className="burger">
+        <div className="burger-bg" onClick={burgerToggle} />
+        <BurgerMenu />
+        <div className="navigation-wrapper no-border">
+          <nav className="navigation-burger">
+            <CloseButton handleClick={burgerToggle} />
+            <ul className="navigation-burger__list" onClick={burgerToggle}>
+              <li className="navigation-burger__item">
+                <NavLink to="/">Главная</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <NavLink to="/about">О нас</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <NavLink to="/services">Услуги</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <NavLink to="/assortment">Ассортимент</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <NavLink to="/our-works">Наши работы</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <NavLink to="/contacts">Контакты</NavLink>
+              </li>
+              <li className="navigation-burger__item">
+                <Instagram width="40" height="40" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+
   const classType =
     type === 'header' || !type ? 'navigation' : 'navigation-footer';
 
   return (
-    <div className={`navigation-wrapper${!type ? '' : ' no-border'}`}>
+    <div
+      className={`navigation-wrapper${type === 'header' ? '' : ' no-border'}`}
+    >
       <nav className={classType}>
         <ul className={`${classType}__list`}>
           <li className={`${classType}__item`}>

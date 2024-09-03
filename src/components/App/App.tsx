ce44f519cd981d3fcff7c998db3821de0b0aee39
so1yana <trapbigest@gmail.com>
2024-09-03 22:04:00 +0300

    services-pages rendering

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -14,6 +14,7 @@ import OurWorks from 'src/pages/ourWorks';
 import Services from 'src/pages/services';
 import Contacts from 'src/pages/contacts';
 import Error from 'src/pages/error';
+import ServicesCardLayout from 'src/pages/servicesCardLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,7 +22,8 @@ const router = createBrowserRouter(
       <Route path="*" element={<Error />} />
       <Route path="/" element={<Main />} />
       <Route path="/about" element={<AboutUs />} />
-      <Route path="/services" element={<Services />} />
+      <Route path="/services" element={<Services />}></Route>
+      <Route path="/services/:id" element={<ServicesCardLayout />} />
       <Route path="/assortment" element={<h2>assortment</h2>} />
       <Route path="/our-works" element={<OurWorks />} />
       <Route path="/contacts" element={<Contacts />} />

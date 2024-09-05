82492d5f57944232ddf012e32e20da83a29eb9f4
so1yana <trapbigest@gmail.com>
2024-09-05 23:37:37 +0300

    added logic and interface changes

--- src/common/PhotoGallery/PhotoGallery.tsx
+++ src/common/PhotoGallery/PhotoGallery.tsx
@@ -1,14 +1,32 @@
 import { FC, useEffect, useState, isValidElement } from 'react';
 
 import './PhotoGallery.scss';
+import observer from 'src/utils/observer.utils';
 
 interface IPhotoGallery {
   inLineCount?: number;
+  photoHeight?: number | string;
   children?: React.ReactNode[] | React.ReactNode;
 }
 
-const PhotoGallery: FC<IPhotoGallery> = ({ children, inLineCount = 3 }) => {
+const PhotoGallery: FC<IPhotoGallery> = ({
+  children,
+  inLineCount = 3,
+  photoHeight = 250,
+}) => {
   const [items, setItems] = useState<React.ReactNode[]>([]);
+
+  useEffect(() => {
+    const componentFirst = document.querySelector('.photo-gallery');
+    const observerFirst =
+      componentFirst && observer(componentFirst, 'smooth-render');
+
+    componentFirst && observerFirst?.observe(componentFirst);
+
+    return () => {
+      observerFirst?.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
     setItems((state) => {
@@ -25,7 +43,10 @@ const PhotoGallery: FC<IPhotoGallery> = ({ children, inLineCount = 3 }) => {
           return (
             <li
               key={String(Math.random())}
-              style={{ width: `calc(100% / ${inLineCount})` }}
+              style={{
+                width: `calc(100% / ${inLineCount})`,
+                height: photoHeight,
+              }}
               className="photo-gallery__photo"
             >
               <div style={{ backgroundImage: `url(${src})`, height: '100%' }} />
@@ -34,7 +55,7 @@ const PhotoGallery: FC<IPhotoGallery> = ({ children, inLineCount = 3 }) => {
         }) || state
       );
     });
-  }, [children, inLineCount]);
+  }, [children, inLineCount, photoHeight]);
 
   return <ul className="photo-gallery">{items}</ul>;
 };

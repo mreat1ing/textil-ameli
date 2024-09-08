d953d4f09467a9fe0b6fd6982a5f0dd81a48b482
so1yana <trapbigest@gmail.com>
2024-09-08 23:20:29 +0300

    some refactoring

---
+++ src/db/assortment.tsx
@@ -0,0 +1,583 @@
+import AssortmentRimskie from 'src/components/AssortmentRimskie';
+import IAssortment from 'src/interfaces/assortment.interface';
+import electro from 'src/assets/img/assormentCards/cornices/electro.png';
+import alluminum from 'src/assets/img/assormentCards/cornices/alluminum.jpg';
+import kovannii from 'src/assets/img/assormentCards/cornices/kovannii.webp';
+import wood from 'src/assets/img/assormentCards/cornices/wood.jpg';
+import exclusive from 'src/assets/img/assormentCards/cornices/exclusive.png';
+import kist from 'src/assets/img/assormentCards/accesories/kist.jpg';
+import podhvat from 'src/assets/img/assormentCards/accesories/podhvat.jpg';
+import magnet from 'src/assets/img/assormentCards/accesories/magnet.jpg';
+import somfy40 from 'src/assets/img/assormentCards/cornices/electro/rim-1.jpg';
+import somfy from 'src/assets/img/assormentCards/cornices/electro/kompakt-s-elektroprivodami-somfy--1-.jpg';
+import compact60electro from 'src/assets/img/assormentCards/cornices/electro/karniz-rimskih-shtor-kompakt-60-s-elektroprivodom-sonesse-ultra-30-wf-rts--1-.jpg';
+import karniz from 'src/assets/img/assormentCards/cornices/electro/elektro-dlya-shtor.png';
+import s1 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-s-1-1.jpg';
+import s2 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-s-2.jpg';
+import l1 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-l-1.jpg';
+import t1 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-t-1.jpg';
+import st2 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-st-2.jpg';
+import st3 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-st-3-1.jpg';
+import glide1 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-glide-1_s-2-2--1-.jpg';
+import glide2 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-glide-2_st-2-1--1-.jpg';
+import glide3 from 'src/assets/img/assormentCards/cornices/alluminium/profilnyj-karniz-glide-3_st-3-1--1-.jpg';
+import vista16 from 'src/assets/img/assormentCards/cornices/kovanie/vista--1-.jpg';
+import swan22 from 'src/assets/img/assormentCards/cornices/kovanie/swan-glavnaya-3--1-.jpg';
+import diy16 from 'src/assets/img/assormentCards/cornices/kovanie/dyv-16-gladkaya-shtanga.jpg';
+import diy28 from 'src/assets/img/assormentCards/cornices/kovanie/diy28.png';
+import sintra from 'src/assets/img/assormentCards/cornices/kovanie/korich-zoloto.jpg';
+import ampir from 'src/assets/img/assormentCards/cornices/kovanie/ampir-1.jpg';
+import gotika from 'src/assets/img/assormentCards/cornices/kovanie/gotika-1.jpg';
+import ost from 'src/assets/img/assormentCards/cornices/kovanie/ost-13.jpg';
+import binario from 'src/assets/img/assormentCards/cornices/derevyannie/temnaya-vishnya.jpg';
+import vintage from 'src/assets/img/assormentCards/cornices/derevyannie/shar-korichneviy-med.jpg';
+import renessans from 'src/assets/img/assormentCards/cornices/derevyannie/temno-zelen---zoloto.jpg';
+import renome19 from 'src/assets/img/assormentCards/cornices/exclusive/liana-2-h-ryadnyj-antik.jpg';
+import selebrity33 from 'src/assets/img/assormentCards/cornices/exclusive/royal-2-h-ryadnyj.jpg';
+import emily19 from 'src/assets/img/assormentCards/cornices/exclusive/emily-roza-2-ryadnyj-antik.jpg';
+import kupel35 from 'src/assets/img/assormentCards/cornices/exclusive/ursula2_img_product_big.png';
+import compactM from 'src/assets/img/assormentCards/rimskie/kompakt-m-erker.jpg';
+import compactLuxe from 'src/assets/img/assormentCards/rimskie/kompakt-lyuks.jpg';
+import compactLip from 'src/assets/img/assormentCards/rimskie/kompakt-lip.jpg';
+import compact60 from 'src/assets/img/assormentCards/rimskie/kompakt-60.jpg';
+import podhvat509 from 'src/assets/img/assormentCards/accesories/podhvati/podhvat-537509-1.jpg';
+import podhvat510 from 'src/assets/img/assormentCards/accesories/podhvati/podhvat-537510-2.jpg';
+import podhvat511 from 'src/assets/img/assormentCards/accesories/podhvati/podhvat-537511-1.jpg';
+import podhvat506 from 'src/assets/img/assormentCards/accesories/podhvati/podhvat-537506-6.jpg';
+import podhvat196 from 'src/assets/img/assormentCards/accesories/podhvati/podhvat-537196-1.jpg';
+import kist800 from 'src/assets/img/assormentCards/accesories/kisti/kist-535800-1.jpg';
+import kist411 from 'src/assets/img/assormentCards/accesories/kisti/kist-537411-1.jpg';
+import kist410 from 'src/assets/img/assormentCards/accesories/kisti/kist-537410-1.jpg';
+import kist233 from 'src/assets/img/assormentCards/accesories/kisti/kist-536233-2.jpg';
+import kist191 from 'src/assets/img/assormentCards/accesories/kisti/kist-536191-1.jpg';
+import magnet463 from 'src/assets/img/assormentCards/accesories/magnets/magnit-537463.jpg';
+import magnet642 from 'src/assets/img/assormentCards/accesories/magnets/magnit-537642.jpg';
+import magnet117 from 'src/assets/img/assormentCards/accesories/magnets/magniti-537117-1.jpg';
+import magnet116 from 'src/assets/img/assormentCards/accesories/magnets/magniti-537116-5.jpg';
+import magnet329 from 'src/assets/img/assormentCards/accesories/magnets/magniti-537329-5.jpg';
+import magnet052 from 'src/assets/img/assormentCards/accesories/magnets/magniti-537052-1.jpg';
+import compactMImg1 from 'src/assets/img/assormentCards/rimskie/compact-m/kompakt-m-1.jpg';
+import compactMImg2 from 'src/assets/img/assormentCards/rimskie/compact-m/kompakt-m-2.jpg';
+import compactMImg3 from 'src/assets/img/assormentCards/rimskie/compact-m/kompakt-m-3.jpg';
+import compactLuxeImg1 from 'src/assets/img/assormentCards/rimskie/compact-luxe/kompakt-lyuks-1.jpg';
+import compactLuxeImg2 from 'src/assets/img/assormentCards/rimskie/compact-luxe/kompakt-lyuks-2.jpg';
+import compactLuxeImg3 from 'src/assets/img/assormentCards/rimskie/compact-luxe/kompakt-lyuks-3.jpg';
+import compactLuxeImg4 from 'src/assets/img/assormentCards/rimskie/compact-luxe/kompakt-lyuks-4.jpg';
+import compactLipImg1 from 'src/assets/img/assormentCards/rimskie/compact-lip/kompakt-lip-1.jpg';
+import compactLipImg2 from 'src/assets/img/assormentCards/rimskie/compact-lip/kompakt-lip-2.jpg';
+import compactLipImg3 from 'src/assets/img/assormentCards/rimskie/compact-lip/kompakt-lip-3.jpg';
+import compact60Img1 from 'src/assets/img/assormentCards/rimskie/compact-60/kompakt-60-1.jpg';
+import compact60Img2 from 'src/assets/img/assormentCards/rimskie/compact-60/kompakt-60-2.jpg';
+
+export const getItems = (where: string, search: string) => {
+  let searchedObject;
+  const listObjects = [assortment, cornicesAssortment, accessoriesAssortment];
+  for (const array of listObjects) {
+    if (searchedObject) break;
+    for (const object of array) {
+      if (object.url === where) {
+        searchedObject = object;
+        break;
+      }
+    }
+  }
+  return searchedObject?.items.find((el) => el.src === search);
+};
+
+const rimskieMekhanizmi = {
+  'compact-m': {
+    descriptionList: [
+      'Минимальная длина карниза для римских штор - 21 см (2 рабочих блока).',
+      'Минимальная длина карниза (без экрера, торцевая часть) для французских штор - 28 см (3 рабочих блока).',
+      'Минимальная длина карниза  (средняя часть карниза) для французских штор - 14 см (1 рабочий блок)',
+      'Максимальная длина карниза (суммарная длина всех частей карниза) - 400 см',
+      'Максимальная высота карнизасо шторой - 500 см',
+      'Максимальное количество эркерных соединителей - 4',
+      'Угол поворота эркерного соединителя - 90-180 градусов',
+      'Максимально возможная нагрузка на механизм (вес шторы с утяжелителями) независимо от количества секций - 4 кг',
+    ],
+    images: [compactMImg1, compactMImg2, compactMImg3],
+  },
+  'compact-luxe': {
+    descriptionList: [
+      'Минимальная длина карниза для римских штор - 30 см (2 рабочих блока).',
+      'Минимальная длина карниза для французских штор - 55 см (3 рабочих блока).',
+      'Максимальная длина карниза (суммарная длина всех частей карниза) - 400 см',
+      'Максимальная высота карниза со шторой - 3,2 м',
+    ],
+    images: [
+      compactLuxeImg1,
+      compactLuxeImg2,
+      compactLuxeImg3,
+      compactLuxeImg4,
+    ],
+  },
+  'compact-lip': {
+    descriptionList: [
+      'Минимальная длина карниза для римских штор - 30 см (2 рабочих блока).',
+      'Минимальная длина карниза для французских штор - 55 см (3 рабочих блока).',
+      'Максимальная длина карниза (суммарная длина всех частей карниза) - 400 см',
+      'Максимальная высота карниза со шторой - 3,2 м',
+    ],
+    images: [compactLipImg1, compactLipImg2, compactLipImg3],
+  },
+  'compact-60': {
+    descriptionList: [
+      'Минимальная длина карниза: зависит от минимального допустимого количества используемых рабочих блоков: для римских штор - 35 см (минимум 2 рабочих блока); для французских штор - 55 см (минимум 3 рабочих блока).',
+      'Максимальная ширина карниза со шторой - 4 м',
+      'Максимальная высота карниза со шторой - 5,5 м',
+      'Минимальная высота карниза со шторой - 0,3 м',
+      'Максимально возможная нагрузка на механизм (вес шторы с утяжелителями на один карниз) независимо от длины карниза - 15 кг',
+    ],
+    images: [compact60Img1, compact60Img2],
+  },
+};
+
+export const assortment: IAssortment[] = [
+  {
+    url: 'karnisy',
+    title: 'Карнизы',
+    items: [
+      {
+        name: 'Электрокарнизы',
+        src: 'elektrokarnisy',
+        image: electro,
+        key: 'Электрокарнизы',
+      },
+      {
+        name: 'Алюминиевые профиля',
+        src: 'alyuminievye-profilya',
+        image: alluminum,
+        key: 'Алюминиевые профиля',
+      },
+      {
+        name: 'Кованые',
+        src: 'kovanie',
+        image: kovannii,
+        key: 'Кованые',
+      },
+      {
+        name: 'Деревянные',
+        src: 'derevyannie',
+        image: wood,
+        key: 'Деревянные',
+      },
+      {
+        name: 'Эксклюзив',
+        src: 'exclusive',
+        image: exclusive,
+        key: 'Эксклюзив',
+      },
+    ],
+  },
+  {
+    url: 'rimskie-mekhanizmy',
+    title: 'Римские механизмы',
+    items: [
+      {
+        name: 'Компакт-M',
+        key: 'Компакт-M',
+        src: 'compact-m',
+        image: compactM,
+        component: (
+          <AssortmentRimskie
+            description={rimskieMekhanizmi['compact-m'].descriptionList}
+            images={rimskieMekhanizmi['compact-m'].images}
+            headerImage={compactM}
+          />
+        ),
+      },
+      {
+        name: 'Компакт-Люкс',
+        key: 'Компакт-Люкс',
+        src: 'compact-luxe',
+        image: compactLuxe,
+        component: (
+          <AssortmentRimskie
+            description={rimskieMekhanizmi['compact-luxe'].descriptionList}
+            images={rimskieMekhanizmi['compact-luxe'].images}
+            headerImage={compactLuxe}
+          />
+        ),
+      },
+      {
+        name: 'Компакт-Лип',
+        key: 'Компакт-Лип',
+        src: 'compact-lip',
+        image: compactLip,
+        component: (
+          <AssortmentRimskie
+            description={rimskieMekhanizmi['compact-lip'].descriptionList}
+            images={rimskieMekhanizmi['compact-lip'].images}
+            headerImage={compactLip}
+          />
+        ),
+      },
+      {
+        name: 'Компакт-60',
+        key: 'Компакт-60',
+        src: 'compact-60',
+        image: compact60,
+        component: (
+          <AssortmentRimskie
+            description={rimskieMekhanizmi['compact-60'].descriptionList}
+            images={rimskieMekhanizmi['compact-60'].images}
+            headerImage={compact60}
+          />
+        ),
+      },
+    ],
+  },
+  {
+    url: 'porternye-tkani',
+    title: 'Портьерные и тюлевые ткани',
+    items: [],
+  },
+  {
+    url: 'accessories',
+    title: 'Аксессуары',
+    items: [
+      {
+        image: podhvat,
+        key: 'Подхваты',
+        name: 'Подхваты',
+        src: 'podhvati',
+      },
+      {
+        image: kist,
+        key: 'Кисти',
+        name: 'Кисти',
+        src: 'kisti',
+      },
+      {
+        image: magnet,
+        key: 'Магниты',
+        name: 'Магниты',
+        src: 'magnets',
+      },
+    ],
+  },
+  {
+    url: 'pokrivala',
+    title: 'Покрывала',
+    items: [],
+  },
+];
+
+export const cornicesAssortment: IAssortment[] = [
+  {
+    url: 'elektrokarnisy',
+    title: 'Электрокарнизы',
+    items: [
+      {
+        image: somfy40,
+        key: 'Компакт 60 с электроприводом SOMFY Sonesse 40',
+        name: 'Компакт 60 с электроприводом SOMFY Sonesse 40',
+        src: 'kompakt-60-s-elektroprivodom-somfy-sonesse-40',
+      },
+      {
+        image: somfy,
+        key: 'Компакт с электроприводами Somfy',
+        name: 'Компакт с электроприводами Somfy',
+        src: 'kompakt-s-elektroprivodami-somfy',
+      },
+      {
+        image: compact60electro,
+        key: 'Карниз римских штор Компакт 60 с электроприводом Sonesse Ultra 30 WF RTS',
+        name: 'Карниз римских штор Компакт 60 с электроприводом Sonesse Ultra 30 WF RTS',
+        src: 'karniz-rimskikh-shtor-kompakt-60-s-elektroprivodom-sonesse-ultra-30-wf-rts',
+      },
+      {
+        image: karniz,
+        key: 'Электрокарнизы для штор',
+        name: 'Электрокарнизы для штор',
+        src: 'elektrokarnizy-dlya-shtor',
+      },
+    ],
+  },
+  {
+    url: 'alyuminievye-profilya',
+    title: 'Алюминиевые профиля',
+    items: [
+      {
+        image: s1,
+        key: 'Профиль С-1',
+        name: 'Профиль С-1',
+        src: 'profil-s-1',
+      },
+      {
+        image: s2,
+        key: 'Профиль С-2',
+        name: 'Профиль С-2',
+        src: 'profil-s-2',
+      },
+      {
+        image: l1,
+        key: 'Профиль Л-1',
+        name: 'Профиль Л-1',
+        src: 'profil-l-1',
+      },
+      {
+        image: t1,
+        key: 'Профиль T-1',
+        name: 'Профиль T-1',
+        src: 'profil-t-1',
+      },
+      {
+        image: st2,
+        key: 'Профиль CT-2',
+        name: 'Профиль CT-2',
+        src: 'profil-st-2',
+      },
+      {
+        image: st3,
+        key: 'Профиль CT-3',
+        name: 'Профиль CT-3',
+        src: 'profil-st-3',
+      },
+      {
+        image: glide1,
+        key: 'GLIDE 1 (Профиль С-2)',
+        name: 'GLIDE 1 (Профиль С-2)',
+        src: 'glide-1-profil-s-2',
+      },
+      {
+        image: glide2,
+        key: 'GLIDE 2 (Профиль СТ-2)',
+        name: 'GLIDE 2 (Профиль СТ-2)',
+        src: 'glide-2-profil-st-2',
+      },
+      {
+        image: glide3,
+        key: 'GLIDE 3 (Профиль СТ-3)',
+        name: 'GLIDE 3 (Профиль СТ-3)',
+        src: 'glide-3-profil-st-3',
+      },
+    ],
+  },
+  {
+    url: 'kovanie',
+    title: 'Кованые',
+    items: [
+      {
+        image: vista16,
+        key: 'Виста Ø16 мм',
+        name: 'Виста Ø16 мм',
+        src: 'vista-16-mm',
+      },
+      {
+        image: swan22,
+        key: 'Swan Ø22 мм',
+        name: 'Swan Ø22 мм',
+        src: 'swan-22-mm',
+      },
+      {
+        image: diy16,
+        key: 'DIY Ø16 мм',
+        name: 'DIY Ø16 мм',
+        src: 'diy-16-mm',
+      },
+      {
+        image: diy28,
+        key: 'DIY Ø28 мм',
+        name: 'DIY Ø28 мм',
+        src: 'diy-28-mm',
+      },
+      {
+        image: sintra,
+        key: 'Синтра',
+        name: 'Синтра',
+        src: 'sintra',
+      },
+      {
+        image: ampir,
+        key: 'Ампир',
+        name: 'Ампир',
+        src: 'ampir',
+      },
+      {
+        image: ost,
+        key: 'ОСТ',
+        name: 'ОСТ',
+        src: 'ost',
+      },
+      {
+        image: gotika,
+        key: 'Готика',
+        name: 'Готика',
+        src: 'gotika',
+      },
+    ],
+  },
+  {
+    url: 'derevyannie',
+    title: 'Деревянные',
+    items: [
+      {
+        image: binario,
+        key: 'Бинарио, Бинарио-Кросс',
+        name: 'Бинарио, Бинарио-Кросс',
+        src: 'binario-kross',
+      },
+      {
+        image: vintage,
+        key: 'Винтаж',
+        name: 'Винтаж',
+        src: 'vintage',
+      },
+      {
+        image: renessans,
+        key: 'Ренессанс',
+        name: 'Ренессанс',
+        src: 'renessans',
+      },
+    ],
+  },
+  {
+    url: 'exclusive',
+    title: 'Эксклюзив',
+    items: [
+      {
+        image: renome19,
+        key: 'Renome Ø19 мм',
+        name: 'Renome Ø19 мм',
+        src: 'renome-19-mm',
+      },
+      {
+        image: selebrity33,
+        key: 'Selebrity Ø33 мм',
+        name: 'Selebrity Ø33 мм',
+        src: 'selebrity-33-mm',
+      },
+      {
+        image: emily19,
+        key: 'Emily Ø19 мм',
+        name: 'Emily Ø19 мм',
+        src: 'emily-19-mm',
+      },
+      {
+        image: kupel35,
+        key: 'Kupel Ø35 мм',
+        name: 'Kupel Ø35 мм',
+        src: 'kupel-35-mm',
+      },
+    ],
+  },
+];
+
+export const accessoriesAssortment: IAssortment[] = [
+  {
+    url: 'podhvati',
+    title: 'Подхваты',
+    items: [
+      {
+        name: 'Подхват 537509',
+        key: 'Подхват 537509',
+        src: 'podhvat-537509',
+        image: podhvat509,
+      },
+      {
+        name: 'Подхват 537510',
+        key: 'Подхват 537510',
+        src: 'podhvat-537510',
+        image: podhvat510,
+      },
+      {
+        name: 'Подхват 537511',
+        key: 'Подхват 537511',
+        src: 'podhvat-537511',
+        image: podhvat511,
+      },
+      {
+        name: 'Подхват 537506',
+        key: 'Подхват 537506',
+        src: 'podhvat-537506',
+        image: podhvat506,
+      },
+      {
+        name: 'Подхват 537196',
+        key: 'Подхват 537196',
+        src: 'podhvat-537196',
+        image: podhvat196,
+      },
+    ],
+  },
+  {
+    url: 'kisti',
+    title: 'Кисти',
+    items: [
+      {
+        name: 'Кисть 535800​',
+        key: 'Кисть 535800​',
+        src: 'kist-535800​',
+        image: kist800,
+      },
+      {
+        name: 'Кисть 537411',
+        key: 'Кисть 537411',
+        src: 'kist-537411',
+        image: kist411,
+      },
+      {
+        name: 'Кисть 537410',
+        key: 'Кисть 537410',
+        src: 'kist-537410',
+        image: kist410,
+      },
+      {
+        name: 'Кисть 536233',
+        key: 'Кисть 536233',
+        src: 'kist-536233',
+        image: kist233,
+      },
+      {
+        name: 'Кисть 536191',
+        key: 'Кисть 536191',
+        src: 'kist-536191',
+        image: kist191,
+      },
+    ],
+  },
+  {
+    url: 'magnets',
+    title: 'Магниты',
+    items: [
+      {
+        name: 'Магнит 537463',
+        key: 'Магнит 537463',
+        src: 'magnet-537463',
+        image: magnet463,
+      },
+      {
+        name: 'Магнит 537642',
+        key: 'Магнит 537642',
+        src: 'magnet-537642',
+        image: magnet642,
+      },
+      {
+        name: 'Магниты 537117',
+        key: 'Магниты 537117',
+        src: 'magnet-537117',
+        image: magnet117,
+      },
+      {
+        name: 'Магниты 537116',
+        key: 'Магниты 537116',
+        src: 'magnet-537116',
+        image: magnet116,
+      },
+      {
+        name: 'Магниты 537329',
+        key: 'Магниты 537329',
+        src: 'magnet-537329',
+        image: magnet329,
+      },
+      {
+        name: 'Магниты 537052',
+        key: 'Магниты 537052',
+        src: 'magnet-537052',
+        image: magnet052,
+      },
+    ],
+  },
+];

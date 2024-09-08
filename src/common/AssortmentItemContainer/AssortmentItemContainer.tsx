d21ea510c611f3a51d11b397e2b0f8ad2863b3b1
so1yana <trapbigest@gmail.com>
2024-09-08 23:22:30 +0300

    wrote main logic for the page

--- src/common/AssortmentItemContainer/AssortmentItemContainer.tsx
+++ src/common/AssortmentItemContainer/AssortmentItemContainer.tsx
@@ -1,9 +1,48 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { NavLink, useLocation, useParams } from 'react-router-dom';
+
+import { getItems } from 'src/db/assortment';
+import observer from 'src/utils/observer.utils';
 
 import './AssortmentItemContainer.scss';
+import AboutUsInvite from '../AboutUsInvite';
 
 const AssortmentItemContainer: FC = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const splittedPath = pathname.split('/');
+  const pathWoId = splittedPath.slice(0, splittedPath.length - 1).join('/');
+  const item = id ? getItems(pathname.split('/')[2], id) : null;
+
+  useEffect(() => {
+    const component = document.querySelector('.services-cards__title');
+    const componentSecond = document.querySelector('.assortment-item__content');
+    const observerFirst = component && observer(component, 'from-left');
+    const observerSecond =
+      componentSecond && observer(componentSecond, 'smooth-render');
+
+    component && observerFirst?.observe(component);
+    componentSecond && observerSecond?.observe(componentSecond);
+
+    return () => {
+      observerFirst?.disconnect();
+      observerSecond?.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="left-background" />
+        <NavLink className="back-link" to={pathWoId}>
+          Назад
+        </NavLink>
+        <h1 className="services-cards__title">{item?.name}</h1>
+        <div className="assortment-item__content">{item?.component}</div>
+        <AboutUsInvite description={false} />
+      </div>
+    </div>
+  );
 };
 
 export default AssortmentItemContainer;

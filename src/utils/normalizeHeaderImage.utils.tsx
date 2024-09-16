3dbec7d9f3f04ad702a9fcbd731dffadb834dafc
so1yana <trapbigest@gmail.com>
2024-09-16 19:36:42 +0300

    initial commit

---
+++ src/utils/normalizeHeaderImage.utils.tsx
@@ -0,0 +1,32 @@
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { SwiperSlide, Swiper } from 'swiper/react';
+
+export const normalizedHeaderImages = (array: string[]) => {
+  const images = array.map((image) => {
+    return (
+      <SwiperSlide key={image}>
+        <img src={image} alt="" />
+      </SwiperSlide>
+    );
+  });
+
+  return (
+    <Swiper
+      className="assortment-item-header__photo"
+      centeredSlides
+      pagination={{
+        clickable: true,
+      }}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      speed={600}
+      modules={[Autoplay, Pagination, Navigation]}
+      navigation
+      loop
+    >
+      {images}
+    </Swiper>
+  );
+};
